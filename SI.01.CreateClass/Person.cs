88a99a8c2cffbf1755cc4fea5d13893f53404427
Richard Hriech <hardric501@gmail.com>
2018-08-03 19:46:05 +0200

    Person class created

---
+++ SI.01.CreateClass/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+
+namespace SI._01.CreateClass
+{
+	public enum Gender
+	{
+		Male, Female
+	}
+
+	internal class Person
+	{
+		public string Name { get; set; }
+		public DateTime BirthDate { get; }
+		public Gender Gender { get; }
+
+		public Person(string name, DateTime birthDate, Gender gender)
+		{
+			Name = name;
+			BirthDate = birthDate;
+			Gender = gender;
+		}
+
+		public override string ToString()
+		{
+			return $"{typeof(Person).Name}; {Name}; {BirthDate.Year}-{BirthDate.Month}-{BirthDate.Day}; {Gender}";
+		}
+
+		private static void Main()
+		{
+			var person = new Person("Jenő", new DateTime(1990, 01, 01), Gender.Male);
+			WriteLine(person);
+			ReadKey();
+		}
+	}
+}
